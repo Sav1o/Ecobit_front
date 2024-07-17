9cc474c044711dc9aae5dcec32f6a62573035dfb
Sávio Araújo Nobre Soares <104694761+Sav1o@users.noreply.github.com>
2024-07-17 14:12:06 -0300

    Delete Back-end-ECOBIT 01.05.24 directory

--- Back-end-ECOBIT 01.05.24/CRUD_20.05/Aulalab/src/main/java/com/example/Aulalab/Model/UsuarioDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.Aulalab.Model;
-import com.example.Aulalab.Model.Usuario;
-
-public record UsuarioDTO(String id, String nome, String email, String telefone, String senha, String cep, String endereco) {
-
-}
